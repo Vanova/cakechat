6c5fffca56b15e2047c74cf343e2c265f868fe09
Vanova <iva427@mail.ru>
2019-03-16 17:03:28 +0800

    fix runner

--- runner.sh
+++ runner.sh
@@ -1,5 +1,22 @@
+#!/bin/bash
+# CakeChat model runner
 
 source activate cake
 export PYTHONPATH="${PYTHONPATH}:$(pwd)"
 
-USE_GP=U0 python tools/train.py
+model=tune
+log_dir=logs/$(date "+%d_%b_%Y")
+log_file=$log_dir/${model}_$(date "+%H_%M_%S").log
+mkdir $log_dir
+
+echo "Log to: $log_file"
+USE_GPU=0 python tools/train.py
+
+# train in dev mode
+model=scratch
+log_dir=logs/$(date "+%d_%b_%Y")
+log_file=$log_dir/${model}_$(date "+%H_%M_%S").log
+mkdir $log_dir
+
+echo "Log to: $log_file"
+IS_DEV=1 python tools/train.py
